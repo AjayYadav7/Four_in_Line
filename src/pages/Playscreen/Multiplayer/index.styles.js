8f6e09a6a00aed5e168cd8150bbd9cec760ed633
ajayyadav7 <ajayyadav2k18@gmail.com>
2022-11-04 00:36:52 +0530

    initial commit

---
+++ src/pages/Playscreen/Multiplayer/index.styles.js
@@ -0,0 +1,11 @@
+const inGameContainer = () => ({
+  width: '100vw',
+  height: '100vh',
+  backgroundolor: 'purple',
+  textAlign: 'center',
+  zIndex: 1,
+})
+
+export default {
+  inGameContainer
+}
